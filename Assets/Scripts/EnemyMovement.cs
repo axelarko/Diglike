57934fae9d2a5b06a71c07af626d0232c31e6760
hermieboi <herman_cool@hotmail.com>
2015-03-12 16:44:56 +0100

    Min första hela commit! :3
    Har addat en "sound map" med olika digsounds samt 2 olika låtar som
    bakgrundsmusik.
    
    Har även addat ett simpelt enemyscript som hoppar fram en unit typ varje
    sekund efter en delay och som dör efter 2 min.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour 
+{
+
+	public float Seconds = 1;
+	public float EnemyDuration = 3;
+	public float EnemyDeath = 120;
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+		if (Time.time >= EnemyDuration) 
+		{
+			transform.position += new Vector3 (1, 0, 0);
+			EnemyDuration = Time.time + Seconds;
+			
+		}
+
+	
+		if (EnemyDuration >= EnemyDeath) 
+		{
+			Destroy(gameObject);
+		}
+
+
+	}
+
+
+}
